55016a46571b876c028a40fa8e98373499e627d4
unknown <erik.moreno0813@gmail.com>
2021-06-01 20:38:01 -0500

    Se agregan películas, se cambia de tamaño la línea divisora

--- main.py
+++ main.py
@@ -6,17 +6,35 @@ class Pelicula:  # CLASE INICIALIZADORA DE PELICULAS, NOMBRE Y GENERO SON DATOS 
         self.precio = precio
 
 #Colocamos en una variable una cantidad predefinida de películas
-cantidad_peliculas = 5
+cantidad_peliculas = 11
 
 # Añadimos las películas
-pelicula1 = Pelicula("Spiderman", "SuperHeroes", 2002, 10.000)  # EJEMPLO DE COMO SE PUEDE CREAR UN OBJETO
-pelicula2 = Pelicula("Venom: Let There Be Carnage", "SuperHeroes", 2021, 20.000)
-pelicula3 = Pelicula("Rapidos y Furiosos 4", "Acción", 2009, 12.000)
-pelicula4 = Pelicula("La caida del Halcón negro", "Acción", 2001, 13.000)
-pelicula5 = Pelicula("Sherlock Holmes", "Aventura", 2009, 15.000)
+pelicula1 = Pelicula("Spiderman.", "SuperHeroes.", 2002, 10.000)  # EJEMPLO DE COMO SE PUEDE CREAR UN OBJETO
+pelicula2 = Pelicula("Venom: Let There Be Carnage.", "SuperHeroes.", 2021, 20.000)
+pelicula3 = Pelicula("Rapidos y Furiosos 4.", "Acción.", 2009, 12.000)
+pelicula4 = Pelicula("La caida del Halcón negro.", "Acción.", 2001, 13.000)
+pelicula5 = Pelicula("Sherlock Holmes.", "Aventura.", 2009, 15.000)
+pelicula6 = Pelicula("Divergente.", "Acción.", 2014, 14.000)
+pelicula7 = Pelicula("Wonder Woman.", "Acción.", 2020, 18.000)
+pelicual8 = Pelicula("Coco.", "Infantil.", 2017, 15.000)
+pelicual9 = Pelicula("Soul.", "Infantil.", 2020, 16.000)
+pelicula10 = Pelicula("El libro de la vida.", "Infantil.", 2014, 12.000)
+pelicula11 = Pelicula("El Conjuro 3: el diablo me obligó a hacerlo. ", "Terror.", 2021, 20.000)
+
+
 #Arreglo para guardar las películas
 ##OBJETOS DE LA PELICULA 5 ACCIÓN/SUPERHEROES, 5 TERROR, 5 NIÑOS/FICCION y 5 DRAMA/COMEDIA
-arr_pelis = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5]
+arr_pelis = [pelicula1,
+             pelicula2,
+             pelicula3,
+             pelicula4,
+             pelicula5,
+             pelicula6,
+             pelicula7,
+             pelicual8,
+             pelicual9,
+             pelicula10,
+             pelicula11]
 
 status = True
 opcion_menu = 0
@@ -29,7 +47,7 @@ def mostrar_peliculas():
     print()
     for i in range(cantidad_peliculas):
         print(f"Película #{i + 1}: Título:", arr_pelis[i].nombre, " Genero:", arr_pelis[i].genero, " Año:", arr_pelis[i].anio, " Valor:", "$", arr_pelis[i].precio)
-        print("-----------------------------------------------------")
+        print("-------------------------------------------------------------------------------------------------------")
 
 '''def final():
     status = False
